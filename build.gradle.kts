056ceb2188dbe53c5bc0c3dceb4241b8ecb5f6ec
vnfflq <vnfflq@gmail.com>
2022-02-04 15:00:18 +0900

    qodanaTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.10"
     application
+    id("org.jetbrains.qodana") version "0.1.13"
 }
 
 group = "me.vnffl"
