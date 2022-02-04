c76d17005ed5b262809a09048ff4b941b91dd30d
vnfflq <vnfflq@gmail.com>
2022-02-04 16:21:55 +0900

    qodanaTest

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,6 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    eeerere
     println("Program arguments: ${args.joinToString()}")
 }
